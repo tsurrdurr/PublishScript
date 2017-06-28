20bf3bed924bc9ca08f6362d35c059670d026084
tsurrdurr <tsurrdurr@users.noreply.github.com>
2017-06-28 19:12:54 +0300

    Added output of copied and omitted files amount

--- publisher.bat
+++ publisher.bat
@@ -1,6 +1,12 @@
-@echo off
+@echo off 
+set copied=0
+set omitted=0
 for /r %1 %%a in (*) do (
     if exist "%2\%%~nxa" (
         copy /y "%%a" %2 >nul
-    ) 
+		set /a "copied+=1"
+    ) else (
+		set /a "omitted+=1"
+	)
 ) 
+echo Files copied: %copied%; Omitted: %omitted%
