44f0bd330a130da0d8714efd401807a799ae8987
tsurrdurr <tsurrdurr@users.noreply.github.com>
2017-07-14 15:58:07 +0300

    Added storing last copied and ommited files amount. Added messages on match and mismatch of last amounts with current ones. Added code generating file with last amounts.

--- publisher.bat
+++ publisher.bat
@@ -1,4 +1,24 @@
 @echo off 
+set CSVFILE=lastresult.txt
+if exist %CSVFILE% (
+	for /F "usebackq tokens=* delims=" %%A in (%CSVFILE%) do (
+    set the_line=%%A
+    call :ProcessLine
+	)	
+) else (
+	copy NUL %CSVFILE%
+)
+goto CopyFiles
+
+:ProcessLine
+for /F "usebackq tokens=1,2,3 delims=~" %%1 in ('%the_line:,=~%') do (
+    set lastcopied=%%1
+    set lastomitted=%%2
+)
+
+goto End
+
+:CopyFiles
 echo Copying files...
 set copied=0
 set omitted=0
@@ -11,4 +31,17 @@ for /r %1 %%a in (*) do (
 	)
 ) 
 echo Files copied: %copied%; Omitted: %omitted%
+echo %copied%,%omitted%,>%CSVFILE% 
+if "%copied%" == "%lastcopied%" (
+	echo Copied same amount as last time
+) else (
+	echo Copied DIFFERENT amount from last time: %lastcopied%
+)
+if "%omitted%" == "%lastomitted%" (
+	echo Omitted same amount as last time
+) else (
+	echo Omitted DIFFERENT amount from last time: %lastomitted%
+)
 pause
+
+:End
